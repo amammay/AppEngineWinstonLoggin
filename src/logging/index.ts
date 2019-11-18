d2199e63450508a0880a39952a57a6b5c279df35
Alex Mammay <alexmammay@gmail.com>
2019-11-17 23:00:01 -0500

    add gcp winston

--- src/logging/index.ts
+++ src/logging/index.ts
@@ -1,30 +1,40 @@
 import winston from 'winston';
 
+// Imports the Google Cloud client library for Winston
+// tslint:disable-next-line:no-var-requires
+const { LoggingWinston } = require('@google-cloud/logging-winston');
+
+const gcpLogger = (id: string) => {
+  return new LoggingWinston({
+    prefix: id
+  });
+};
+
 const loggerCreator = (id: string) => {
-  return winston.createLogger({
+
+  const log = winston.createLogger({
     level: 'info',
     defaultMeta: { logRoot: id },
     levels: winston.config.syslog.levels,
     transports: []
   });
+  if (process.env.NODE_ENV !== 'production') {
+    log.add(new winston.transports.Console({
+      level: 'debug',
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      )
+    }));
+  } else {
+    log.add(gcpLogger(id));
+  }
+
+  return log;
 };
 
 const logger = loggerCreator('general');
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    level: 'debug',
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.simple()
-    )
-  }));
-} else {
-  logger.add(new winston.transports.Console({
-    level: 'info',
-    format: winston.format.json()
-  }));
-}
 const stream = {
   write: (message: any) => {
     logger.info(message.substring(0, message.lastIndexOf('\n')));
